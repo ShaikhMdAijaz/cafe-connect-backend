420047b61b3576d1a9c450a0da96819cb2817dc2
ShaikhMdAijaz <shaikhmdaijazprsnl12@gmail.com>
2025-08-30 01:07:53 +0530

    UserCart,UserAddress PATCH and DELETE Api

--- index.js
+++ index.js
@@ -68,11 +68,11 @@ app.get('/api/menu', async (req, res) => {
     //const result = await pool.query('SELECT * FROM get_menu_list() ORDER BY "SeqNo" DESC');
     const result = await pool.query('SELECT * FROM getmenulist()');
     //res.json(result.rows);
-    res.status(200).json({statusCode: 200,data: result.rows,error: ''});
-  } 
+    res.status(200).json({ statusCode: 200, data: result.rows, error: '' });
+  }
   catch (error) {
     console.error('Error fetching menu:', error);
-    res.status(500).json({ statusCode: 500,error: error });
+    res.status(500).json({ statusCode: 500, error: error });
   }
 });
 
@@ -124,7 +124,7 @@ app.get('/api/user/:valueID', async (req, res) => {
 
   if (!valueID) {
     return res.status(400).json({
-      statusCode: 400,data: {},error: 'Missing valueID (email or mobile number)',
+      statusCode: 400, data: {}, error: 'Missing valueID (email or mobile number)',
     });
   }
 
@@ -136,7 +136,7 @@ app.get('/api/user/:valueID', async (req, res) => {
     }
     else {
       return res.status(400).json({
-        statusCode: 400,data: {},error: 'Invalid email or mobile number format',
+        statusCode: 400, data: {}, error: 'Invalid email or mobile number format',
       });
     }
 
@@ -144,17 +144,17 @@ app.get('/api/user/:valueID', async (req, res) => {
 
     if (result.rows.length == 0) {
       return res.status(200).json({
-        statusCode: 400,data: {},error: 'User not found',result : result
+        statusCode: 400, data: {}, error: 'User not found', result: result
       });
     }
 
     res.status(200).json({
-      statusCode: 200,data: result.rows[0],error: '',
+      statusCode: 200, data: result.rows[0], error: '',
     });
-  } 
+  }
   catch (error) {
     console.error('Error fetching user:', error);
-    res.status(500).json({statusCode: 500, data: {}, error: error });
+    res.status(500).json({ statusCode: 500, data: {}, error: error });
   }
 });
 
@@ -200,14 +200,14 @@ app.get('/api/user/:valueID', async (req, res) => {
  *         description: Missing or invalid parameters
  */
 
-app.post('/api/login',async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { userName, userMobileNo, otp, token, email } = req.body;
 
   if (!userName || !userMobileNo || !otp || !token) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-  try{
+  try {
     // Simulate insertion (in real app, insert into DB here)
     // const newUser = {
     //   userName,
@@ -217,20 +217,20 @@ app.post('/api/login',async (req, res) => {
     //   email: email || null,
     // };
 
-     const  result  = await pool.query(
-      `SELECT * FROM insertuser($1,$2,$3,$4,$5)`,[userName, userMobileNo, otp, token, email]
+    const result = await pool.query(
+      `SELECT * FROM insertuser($1,$2,$3,$4,$5)`, [userName, userMobileNo, otp, token, email]
     );
-  
-    if((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].id == 0){
-      return res.status(200).json({statusCode: 400,data: {},error: result.rows[0].errorMsg,result : result});
+
+    if ((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].id == 0) {
+      return res.status(200).json({ statusCode: 400, data: {}, error: result.rows[0].errorMsg, result: result });
     }
-    return res.status(200).json({statusCode: 200 ,message: 'User data inserted successfully', data: result.rows[0], error : '' });
+    return res.status(200).json({ statusCode: 200, message: 'User data inserted successfully', data: result.rows[0], error: '' });
     //console.log('User inserted:', newUser);  
     //return res.status(200).json({statusCode: 200 ,message: 'User data inserted successfully', data: newUser });
   }
   catch (error) {
     console.error('Error fetching menu:', error);
-    res.status(500).json({ statusCode: 500 , error: error });
+    res.status(500).json({ statusCode: 500, error: error });
   }
 });
 
@@ -256,18 +256,18 @@ app.post('/api/login',async (req, res) => {
 app.get('/api/state/filllist', async (req, res) => {
   try {
     //const status = req.query.status || 1;
-    const  result  = await pool.query('SELECT * FROM getfilllist()');
+    const result = await pool.query('SELECT * FROM getfilllist()');
     //res.json(rows);
     if (result.rows.length == 0) {
       return res.status(200).json({
-        statusCode: 400,data: {},error: 'States list not found',result : result
+        statusCode: 400, data: {}, error: 'States list not found', result: result
       });
     }
-    res.status(200).json({statusCode: 200, data: result.rows, error: '',result : result});
-  } 
+    res.status(200).json({ statusCode: 200, data: result.rows, error: '', result: result });
+  }
   catch (err) {
     console.error(err);
-    res.status(500).json({statusCode: 500, data:{} ,error: err });
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
   }
 });
 
@@ -305,21 +305,21 @@ app.get('/api/user/address/:userID', async (req, res) => {
   const { userID } = req.params;
 
   if (!userID || isNaN(userID)) {
-    return res.status(200).json({ statusCode: 400, data: {},error: 'Invalid user ID' });
+    return res.status(200).json({ statusCode: 400, data: {}, error: 'Invalid user ID' });
   }
 
   try {
-    const result = await pool.query(`SELECT * FROM getusersaddressdtl($1)`,[userID]);
-      if (result.rows.length == 0) {
+    const result = await pool.query(`SELECT * FROM getusersaddressdtl($1)`, [userID]);
+    if (result.rows.length == 0) {
       return res.status(200).json({
-        statusCode: 400,data: {},error: 'User Address not found',result:result
+        statusCode: 400, data: {}, error: 'User Address not found', result: result
       });
     }
-    res.status(200).json({statusCode: 200, data: result.rows, error: '',});
-  } 
+    res.status(200).json({ statusCode: 200, data: result.rows, error: '', });
+  }
   catch (err) {
     console.error('Error fetching user address:', err);
-    res.status(500).json({ statusCode: 500, data: {},error: err });
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
   }
 });
 
@@ -354,20 +354,20 @@ app.get('/api/user/address/:userID', async (req, res) => {
 app.get('/api/user/cart/:userID', async (req, res) => {
   const { userID } = req.params;
   if (!userID || isNaN(userID)) {
-    return res.status(200).json({ statusCode: 400, data: {},error: 'Invalid user ID' });
+    return res.status(200).json({ statusCode: 400, data: {}, error: 'Invalid user ID' });
   }
   try {
     const result = await pool.query('SELECT * FROM getuserscartlist($1)', [userID]);
     if (result.rows.length == 0) {
       return res.status(200).json({
-        statusCode: 400,data: {},error: 'User Cart not found',result:result
+        statusCode: 400, data: {}, error: 'User Cart not found', result: result
       });
     }
-    res.status(200).json({statusCode: 200, data: result.rows, error: '',});
-  } 
+    res.status(200).json({ statusCode: 200, data: result.rows, error: '', });
+  }
   catch (err) {
     console.error(err);
-    res.status(500).json({statusCode: 500, data: {},error: err });
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
   }
 });
 
@@ -431,9 +431,9 @@ app.get('/api/user/cart/:userID', async (req, res) => {
  *                   type: integer
  */
 app.post('/api/users/cart', async (req, res) => {
-  const {userID,menuID,type,subType,code,description,rate,qty,image,seqNo,} = req.body;
-  if ((!userID || isNaN(userID)) || (!menuID || isNaN(menuID)) || !type || !subType || !code || !description || (!rate || isNaN(rate))|| (!qty || isNaN(qty))|| !image) {
-    return res.status(400).json({statusCode: 400,error: 'Missing required fields' });
+  const { userID, menuID, type, subType, code, description, rate, qty, image, seqNo, } = req.body;
+  if ((!userID || isNaN(userID)) || (!menuID || isNaN(menuID)) || !type || !subType || !code || !description || (!rate || isNaN(rate)) || (!qty || isNaN(qty)) || !image) {
+    return res.status(400).json({ statusCode: 400, error: 'Missing required fields' });
   }
   try {
     const result = await pool.query(
@@ -442,14 +442,132 @@ app.post('/api/users/cart', async (req, res) => {
     );
 
     //res.json(result.rows[0]); // Will return { errorMsg, usersCartDtlID }    
-    if((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].usersCartDtlID == 0){
-      return res.status(200).json({statusCode: 400,data: {},error: result.rows[0].errorMsg,result : result});
+    if ((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].usersCartDtlID == 0) {
+      return res.status(200).json({ statusCode: 400, data: {}, error: result.rows[0].errorMsg, result: result });
     }
-    return res.status(200).json({statusCode: 200 ,message: 'User Cart inserted successfully', data: result.rows[0], error : '' });
-  } 
+    return res.status(200).json({ statusCode: 200, message: 'User Cart inserted successfully', data: result.rows[0], error: '' });
+  }
   catch (err) {
     console.error(err);
-    res.status(500).json({ statusCode: 500, data: {},error: err });
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
+  }
+});
+
+//PATCH API for UserCartDtl
+/**
+ * @swagger
+ * /api/users/cart/{id}:
+ *   patch:
+ *     summary:
+ *     tags: [User Cart]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userID:
+ *                 type: integer
+ *               menuID:
+ *                 type: integer
+ *               type:
+ *                 type: string
+ *               subType:
+ *                 type: string
+ *               code:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               rate:
+ *                 type: integer
+ *               qty:
+ *                 type: integer
+ *               image:
+ *                 type: string
+ *               seqNo:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Cart updated successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+app.patch("/api/users/cart/:id", async (req, res) => {
+  const { id } = req.params;
+  const { userID, menuID, type, subType, code, description, rate, qty, image, seqNo } = req.body;
+
+  // Basic validations
+  if ((!id || isNaN(id)) || (!userID || isNaN(userID)) || (!menuID || isNaN(menuID)) || !type || !subType || !code || !description || (!rate || isNaN(rate)) || (!qty || isNaN(qty)) || !image || (!seqNo || isNaN(seqNo))) {
+    return res.status(400).json({ statusCode: 400, error: 'Missing or invalid required fields' });
+  }
+
+  try {
+    const result = await pool.query(
+      `SELECT * FROM updateuserscartdtl($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`,
+      [id, userID, menuID, type, subType, code, description, rate, qty, image, seqNo]
+    );
+
+    if (result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) {
+      return res.status(200).json({ statusCode: 400, data: {}, error: result.rows[0].errorMsg, result: result });
+    }
+    return res.status(200).json({ statusCode: 200, message: '"Cart updated successfully', error: '' });
+    // if (result.rows[0]?.errorMsg) {
+    //   return res.status(400).json({statusCode: 400,error: result.rows[0].errorMsg,data: {}});
+    // }
+    // return res.status(200).json({statusCode: 200,message: "Cart updated successfully",error: ""});
+
+  } 
+  catch (err) {
+    console.error("Error updating cart:", err);
+    res.status(500).json({statusCode: 500,data: {},error: err.message || err});
+  }
+});
+
+//DELETE API for UserCartDtl
+/**
+ * @swagger
+ * /api/users/cart/{id}/{userID}:
+ *   delete:
+ *     summary:
+ *     tags: [User Cart]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description:
+ *       - in: path
+ *         name: userID
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: User ID
+ *     responses:
+ *       200:
+ *         description: Cart item(s) deleted successfully
+ *       400:
+ *         description: Missing or invalid parameters
+ *       500:
+ *         description: Internal server error
+ */
+app.delete("/api/users/cart/:id/:userID", async (req, res) => {
+  const { id, userID } = req.params;
+
+  if (!id || isNaN(id) || !userID || isNaN(userID)) {
+    return res.status(400).json({statusCode: 400,error: "Missing or invalid parameters"});
+  }
+
+  try {
+    // Call your PL/pgSQL function
+    await pool.query(`SELECT deleteuserscartdtl($1, $2)`, [id, userID]);
+    return res.status(200).json({ statusCode: 200, message: '', error: '' });
+  } 
+  catch (err) {
+    return res.status(500).json({statusCode: 500,data: {},error: err.message});
   }
 });
 
@@ -516,31 +634,110 @@ app.post('/api/users/cart', async (req, res) => {
  *                   type: integer
  */
 app.post('/api/users/address', async (req, res) => {
-  const {userID, addressType, fullName, mobileNo, pincode,houseNo, area, landmark, city, state, isDefaultAddress} = req.body;
+  const { userID, addressType, fullName, mobileNo, pincode, houseNo, area, landmark, city, state, isDefaultAddress } = req.body;
   if ((!userID || isNaN(userID)) || !addressType || !fullName || !mobileNo || !pincode || !houseNo || !area || !landmark || !city || !state || !isDefaultAddress) {
-    return res.status(400).json({statusCode: 400,error: 'Missing required fields' });
+    return res.status(400).json({ statusCode: 400, error: 'Missing required fields' });
   }
   try {
     // Insert into UsersAddressDtl
-   const result =  await pool.query(`
+    const result = await pool.query(`
       SELECT * FROM insertusersaddressdtl($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
     `, [userID, addressType, fullName, mobileNo, pincode,
       houseNo, area, landmark, city, state, isDefaultAddress ? '1' : '0']
-  );
+    );
 
     // Fetch updated list using your function
     //const { rows } = await pool.query(`SELECT * FROM getusersaddressdtl($1)`, [userID]);
-    if((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].usersAddressDtlID == 0){
-      return res.status(200).json({statusCode: 400,data: {},error: result.rows[0].errorMsg,result : result});
+    if ((result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) || result.rows[0].usersAddressDtlID == 0) {
+      return res.status(200).json({ statusCode: 400, data: {}, error: result.rows[0].errorMsg, result: result });
     }
     //res.json({success: true,message: "Address added successfully",addresses: rows});
-    return res.status(200).json({statusCode: 200 ,message: 'Address inserted successfully', data: result.rows[0], error : '' });
+    return res.status(200).json({ statusCode: 200, message: 'Address inserted successfully', data: result.rows[0], error: '' });
 
-  } 
+  }
   catch (err) {
     //console.error("Error inserting address:", err.message);
     //res.status(500).json({success: false,error: err.message});
-    res.status(500).json({ statusCode: 500, data: {},error: err });
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
+  }
+});
+
+//PATCH API for useraddressdtl
+/**
+ * @swagger
+ * /api/users/address/{id}:
+ *   patch:
+ *     summary: 
+ *     tags: [User Address]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userID:
+ *                 type: integer
+ *               addressType:
+ *                 type: string
+ *               fullName:
+ *                 type: string
+ *               mobileNo:
+ *                 type: string
+ *               pincode:
+ *                 type: string
+ *               houseNo:
+ *                 type: string
+ *               area:
+ *                 type: string
+ *               landmark:
+ *                 type: string
+ *               city:
+ *                 type: string
+ *               state:
+ *                 type: string
+ *               isDefaultAddress:
+ *                 type: boolean
+ *               isDelete:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Address updated successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+app.patch("api/users/address/:id", async (req, res) => {
+  const { id } = req.params;
+  const { userID, addressType, fullName, mobileNo, pincode, houseNo, area, landmark, city, state, isDefaultAddress, isDelete, } = req.body;
+  if ((!id || isNaN(id)) || (!userID || isNaN(userID)) || !addressType || !fullName || !mobileNo || !pincode || !houseNo || !area || !landmark || !city || !state || !isDefaultAddress || !isDelete) {
+    return res.status(400).json({ statusCode: 400, error: 'Missing required fields' });
+  }
+  try {
+    const result = await pool.query(`
+      SELECT * FROM updateusersaddressdtl($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+    `, [id,userID,addressType,fullName,mobileNo,pincode,houseNo,area,landmark,
+      city,state,isDefaultAddress ? '1' : '0', isDelete ? '1' : '0']
+    );
+
+    //const { rows } = await pool.query(query, values);
+
+    if (result.rows[0].errorMsg != '' && result.rows[0].errorMsg != null) {
+      return res.status(200).json({ statusCode: 400, data: {}, error: result.rows[0].errorMsg, result: result });
+    }
+    //res.json({success: true,message: "Address added successfully",addresses: rows});
+    return res.status(200).json({ statusCode: 200, message: 'Address updated successfully', error: '' });
+
+    // if (rows[0]?.errorMsg) {
+    //   return res.status(400).json({ success: false, message: rows[0].errorMsg });
+    // }
+
+    // res.json({ success: true, message: "Address updated successfully" });
+  }
+  catch (err) {
+    //console.error("Error updating address:", err);
+    res.status(500).json({ statusCode: 500, data: {}, error: err });
   }
 });
 
